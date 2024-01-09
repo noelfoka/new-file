7d160971b210cbc91aedcbedfb87dee13fca4db9
noelfoka <nomgnenoel@gmail.com>
2024-01-09 14:35:51 +0100

    constant

--- cours.js
+++ cours.js
@@ -2,8 +2,11 @@ let prenom = "Je m'appelle Noel";
 let age = 33;
 let age2 = "33";
 
-document.getElementById("p1").innerHTML = "Type de prenom " + typeof prenom;
-document.getElementById("p2").innerHTML = "Type de age" + 
+document.getElementById("p1").innerHTML = "Type de prenom: " + typeof prenom;
+document.getElementById("p2").innerHTML = "Type de age: " + 
 typeof age;
-document.getElementById("p3").innerHTML = "Type de age2" + 
+document.getElementById("p3").innerHTML = "Type de age2: " + 
 typeof age2;
+
+const a = "Salut";
+const b = "les gents";
